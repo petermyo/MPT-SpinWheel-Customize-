82ac0abc7eb794accd44b9111e91e23165af8b3e
petermyo <p3t3rmyo@gmail.com>
2026-02-24 09:36:54 +0630

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -10,9 +10,9 @@ export const COLORS = {
 };
 
 export const INITIAL_SLICES: SpinSlice[] = [
-  { id: '1', title: '500MB', amount: '1500', icon: '📶', chance: 50, color: COLORS.MPT_YELLOW },
-  { id: '2', title: '1 GB', amount: '3000', icon: '🌐', chance: 9, color: COLORS.MPT_BLUE },
-  { id: '1', title: '500MB', amount: '1500', icon: '📶', chance: 25, color: COLORS.MPT_YELLOW },
-  { id: '2', title: '1 GB', amount: '3000', icon: '🌐', chance: 8, color: COLORS.MPT_BLUE },
-  { id: '4', title: '5 GB', amount: '15000', icon: '⚡', chance: 8, color: COLORS.WHITE },
+  { id: '1', title: '500MB', amount: '7400', icon: '📶', chance: 50, color: COLORS.MPT_YELLOW },
+  { id: '2', title: '1 GB', amount: '15000', icon: '🌐', chance: 9, color: COLORS.MPT_BLUE },
+  { id: '1', title: '500MB', amount: '7400', icon: '📶', chance: 25, color: COLORS.MPT_YELLOW },
+  { id: '2', title: '1 GB', amount: '15000', icon: '🌐', chance: 8, color: COLORS.MPT_BLUE },
+  { id: '4', title: '5 GB', amount: '75000', icon: '⚡', chance: 8, color: COLORS.WHITE },
 ];
